39ce5d386d8030a34adfe22ed14efd490a6975d4
Vladimir Baikalov <deadinside@rec-a100.sas.yp-c.yandex.net>
2025-05-03 01:26:35 +0300

    Change files location and simplify structure

---
+++ configs/gowalla_other.py
@@ -0,0 +1,14 @@
+from config import GSASRecExperimentConfig
+
+config = GSASRecExperimentConfig(
+    dataset_name='gowalla',
+    sequence_length=200,
+    embedding_dim=128,
+    num_heads=1,
+    max_batches_per_epoch=100,
+    num_blocks=2,
+    dropout_rate=0.5,
+    negs_per_pos=256,
+    path_to_cnt='./datasets/gowalla/item_cnt.pkl',
+    filter_rated=False
+)
